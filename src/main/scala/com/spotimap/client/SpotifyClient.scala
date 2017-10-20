e11ae67220067e48bb1df897ab04b8592cc5ad1f
Jakub Kozłowski <kubukoz@gmail.com>
2017-10-20 19:14:40 +0200

    Add POST request support in SpotifyClient

--- src/main/scala/com/spotimap/client/SpotifyClient.scala
+++ src/main/scala/com/spotimap/client/SpotifyClient.scala
@@ -2,42 +2,70 @@ package com.spotimap.client
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpMethods.GET
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.HttpMethods.{GET, POST}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.{Authorization, OAuth2BearerToken}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.Materializer
+import cats.Monad
+import cats.instances.future._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
 import com.spotimap.Main
+import com.spotimap.Main.Result
 import com.spotimap.model.external.SpotifyToken
 import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
-import io.circe.Decoder
+import io.circe.{Decoder, Encoder}
 
 import scala.concurrent.ExecutionContext
 import scala.language.higherKinds
 
-abstract class SpotifyClient[F[_]] {
-  private[client] def get[T: Decoder](path: String, absolute: Boolean = false)
-                                     (implicit token: SpotifyToken): F[T] = {
-    httpCallRaw[T](GET, path, absolute)
+abstract class SpotifyClient[+F[_] : Monad] {
+  protected type Serialized
+
+  protected def serialize[T: Encoder]: T => F[Serialized]
+
+  private[client] def get[Response: Decoder](path: String, absolute: Boolean = false)
+                                            (implicit token: SpotifyToken): F[Response] = {
+    httpCallRaw[Response](GET, path, absolute, body = None)
   }
 
-  protected def httpCallRaw[T: Decoder](method: HttpMethod, path: String, absolute: Boolean)
-                                     (implicit token: SpotifyToken): F[T]
+  private[client] def post[Request: Encoder, Response: Decoder](path: String, body: Request, absolute: Boolean = false)
+                                                               (implicit token: SpotifyToken): F[Response] = {
+    for {
+      entity <- serialize(Encoder[Request])(body)
+      result <- httpCallRaw[Response](POST, path, absolute, body = Some(entity))
+    } yield result
+  }
+
+  protected def httpCallRaw[T: Decoder](method: HttpMethod, path: String, absolute: Boolean, body: Option[Serialized])
+                                       (implicit token: SpotifyToken): F[T]
 }
 
 class SpotifyClientImpl(implicit system: ActorSystem, am: Materializer, ec: ExecutionContext)
-  extends SpotifyClient[Main.Result]{
+  extends SpotifyClient[Main.Result] {
+
+  override protected type Serialized = RequestEntity
+
+  override protected def serialize[T: Encoder]: T => Result[RequestEntity] =
+    Marshal(_).to[RequestEntity]
 
   private val http = Http()
 
-  override protected def httpCallRaw[T: Decoder](method: HttpMethod, path: String, absolute: Boolean)
-                                     (implicit token: SpotifyToken): Main.Result[T] = {
-    val getUrl: String => String = if(absolute) identity else fullUrl
+  override protected def httpCallRaw[T: Decoder](method: HttpMethod, path: String, absolute: Boolean, entity: Option[RequestEntity])
+                                                (implicit token: SpotifyToken): Main.Result[T] = {
+    val getUrl: String => String = if (absolute) identity else fullUrl
 
     val authorization = Authorization(OAuth2BearerToken(token.value))
     val headers: List[HttpHeader] = List(authorization)
 
-    val request = HttpRequest(method, Uri.parseAbsolute(getUrl(path)), headers)
+    val request = HttpRequest(
+      method = method,
+      uri = Uri.parseAbsolute(getUrl(path)),
+      headers = headers,
+      entity = entity.getOrElse(HttpEntity.Empty)
+    )
 
     for {
       response <- http.singleRequest(request)
