20eab4d5c39ed37bc1d4a0ab85077d1a5c3e2e35
Jakub Kozłowski <kubukoz@gmail.com>
2017-10-21 22:53:43 +0200

    Remove HttpClient variance since we're not using it anyways

--- src/main/scala/com/spotimap/client/impl/HttpClient.scala
+++ src/main/scala/com/spotimap/client/impl/HttpClient.scala
@@ -6,7 +6,7 @@ import io.circe.{Decoder, Encoder}
 
 import scala.language.higherKinds
 
-abstract class HttpClient[+F[_]: Monad] {
+abstract class HttpClient[F[_]: Monad] {
   private[client] def serialize[T: Encoder](input: T): F[RequestEntity]
 
   private[client] def httpCallRaw[T: Decoder](method: HttpMethod,
